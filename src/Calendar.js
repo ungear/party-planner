0c6d83be39600cc74ecf7b4ee38a31ca79ac7d22
Mikhail Istomin <mikhail_istomin@epam.com>
2019-01-23 11:46:52 +0300

    show weekdays

--- src/Calendar.js
+++ src/Calendar.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import DayCard from "./DayCard";
 import "./Calendar.css";
 
+const Weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
 export default class Calendar extends Component {
   render() {
     let calendar = getCalendar(this.props.date.year, this.props.date.month);
-    let daysMarkup = calendar.map(d => <DayCard key={d.dayNumber} dayInfo={d} />)
+    let daysMarkup = calendar.map(d =>
+      <div
+        className="c-cell c-cell--card"
+        key={d.dayNumber}
+        style={{ "gridColumn": d.weekDay + 1 }}
+      >
+        <DayCard dayInfo={d} />
+      </div>
+    )
+    let header = Weekdays.map((v, i) =>
+      <div className="c-cell c-cell--header" key={i}>{v}</div>
+    )
     return (
       <section className="calendar">
+        {header}
         {daysMarkup}
       </section>
     )

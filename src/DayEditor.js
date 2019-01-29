f7869466b27e7fb8b7961c99e590006df8e5e71b
Mikhail Istomin <redeemer@inbox.ru>
2019-01-29 23:20:43 +0300

    delete event part 1; prettier

--- src/DayEditor.js
+++ src/DayEditor.js
@@ -1,37 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Event from "./Event";
 
 export default class DayEditor extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       events: [...props.events]
-    }
+    };
     this.onEventUpdated = this.onEventUpdated.bind(this);
-    this.onSaveClick = this.onSaveClick.bind(this)
+    this.onSaveClick = this.onSaveClick.bind(this);
+    this.onEventDeleted = this.onEventDeleted.bind(this);
   }
 
   onEventUpdated(updatedEvent) {
     let eventsList = [...this.state.events];
-    let eventIndex = eventsList.findIndex(x => x.eventId === updatedEvent.eventId);
+    let eventIndex = eventsList.findIndex(
+      x => x.eventId === updatedEvent.eventId
+    );
     eventsList[eventIndex] = updatedEvent;
-    this.setState({ ...this.state, events: eventsList })
+    this.setState({ ...this.state, events: eventsList });
   }
 
   onSaveClick() {
-    this.props.onEventsUpdated(this.state.events)
+    this.props.onEventsUpdated(this.state.events);
   }
 
+  onEventDeleted(eventToDelete) {}
   render() {
-    let dateString = new Date(this.props.dayTimestamp).toLocaleString("en-US", { month: 'long', year: 'numeric', day: "numeric" })
-    let eventsData = this.props.events.filter(e => e.time === this.props.dayTimestamp);
-    let eventsMarkup = eventsData.map(e => <Event key={e.eventId} event={e} onEventUpdated={this.onEventUpdated} />)
+    let dateString = new Date(this.props.dayTimestamp).toLocaleString("en-US", {
+      month: "long",
+      year: "numeric",
+      day: "numeric"
+    });
+    let eventsData = this.props.events.filter(
+      e => e.time === this.props.dayTimestamp
+    );
+    let eventsMarkup = eventsData.map(e => (
+      <Event
+        key={e.eventId}
+        event={e}
+        onEventUpdated={this.onEventUpdated}
+        onEventDeleted={this.onEventDeleted}
+      />
+    ));
     return (
       <section>
         <h5>{dateString}</h5>
         <ul>{eventsMarkup}</ul>
-        <button type="button" onClick={this.onSaveClick}>Save</button>
+        <button type="button" onClick={this.onSaveClick}>
+          Save
+        </button>
       </section>
-    )
+    );
   }
 }
--- src/Event.js
+++ src/Event.js
@@ -1,30 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Event extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       eventId: props.event.eventId,
       time: props.event.time,
       description: props.event.description
-    }
+    };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.onDeleteButtonClick = this.onDeleteButtonClick.bind(this);
   }
 
   handleInputChange(event) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    this.setState({
-      [name]: value
-    }, _ => this.props.onEventUpdated(this.state));
+    this.setState(
+      {
+        [name]: value
+      },
+      _ => this.props.onEventUpdated(this.state)
+    );
+  }
+
+  onDeleteButtonClick() {
+    this.props.onEventDeleted({ ...this.state });
   }
 
   render() {
     return (
       <div>
-        <input value={this.state.description} name="description" onChange={this.handleInputChange} ></input>
+        <input
+          value={this.state.description}
+          name="description"
+          onChange={this.handleInputChange}
+        />
+        <button onClick={this.onDeleteButtonClick}>X</button>
       </div>
-    )
+    );
   }
 }
